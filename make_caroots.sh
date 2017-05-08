1498c782f00830f29ec05f7e769fe660d1fb9a21
Rob Stradling <rob@comodo.com>
2017-05-09 00:20:12 +0100

    Add column headers to TSV files.

--- make_caroots.sh
+++ make_caroots.sh
@@ -5,7 +5,8 @@ function make_caroots_pem {
 }
 
 function make_caroots_tsv {
-	find crt/common crt/$1 -iname *.crt -printf %f -exec openssl x509 -nameopt utf8 -subject -noout -in '{}' ';' 2>/dev/null | sed "s/\.crtsubject= /\t/g" | sort > tsv/$1-ca-roots.tsv
+	echo "SHA-256(Certificate)	CA Name" > tsv/$1-ca-roots.tsv
+	find crt/common crt/$1 -iname *.crt -printf %f -exec openssl x509 -nameopt utf8 -subject -noout -in '{}' ';' 2>/dev/null | sed "s/\.crtsubject= /\t/g" | sort >> tsv/$1-ca-roots.tsv
 }
 
 make_caroots_pem dodo
