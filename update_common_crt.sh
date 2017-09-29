5792e9be10b84b5fb273b4272fa13cd5e3553957
Rob Stradling <rob@comodo.com>
2017-09-29 22:05:40 +0100

    Tweak some comments.

--- update_common_crt.sh
+++ update_common_crt.sh
@@ -3,7 +3,7 @@
 cd crt/common/trusted
 rm *.crt
 
-# Get a list (from the crt.sh DB) of the SHA-256 hashes of all of the roots that are currently trusted by the Apple, Microsoft and Mozilla root programs.
+# Get a list (from the crt.sh DB) of the SHA-256 hashes of all of the roots that are currently enabled for the server authentication trust purpose in one or more of the Apple, Microsoft and Mozilla root programs.
 # TODO: Expose this data via https://crt.sh/
 psql -c "\COPY (SELECT upper(encode(digest(c.CERTIFICATE, 'sha256'), 'hex')) FROM root_trust_purpose rtp, ca_certificate cac, certificate c WHERE rtp.TRUST_CONTEXT_ID IN (1, 5, 12) AND rtp.TRUST_PURPOSE_ID = 1 and rtp.CERTIFICATE_ID = cac.CERTIFICATE_ID AND cac.CERTIFICATE_ID = c.ID GROUP BY digest(c.CERTIFICATE, 'sha256') ORDER BY min(get_ca_name_attribute(cac.CA_ID))) TO '__roots_sha256__.sh'"
 
--- update_dodo_crt.sh
+++ update_dodo_crt.sh
@@ -2,7 +2,7 @@
 
 cd crt/dodo
 
-# Get a list (from the crt.sh DB) of the SHA-256 hashes of all of the roots that are currently trusted (but not for the server authentication trust purpose)by the Apple, Microsoft and Mozilla root programs.  These roots will be accepted by Dodo but not by Mammoth or Sabre.
+# Get a list (from the crt.sh DB) of the SHA-256 hashes of all of the roots that are currently trusted (but not for the server authentication trust purpose) by one or more of the Apple, Microsoft and Mozilla root programs.  These roots will be accepted by Dodo but not by Mammoth or Sabre.
 # TODO: Expose this data via https://crt.sh/
 psql -c "\COPY (SELECT upper(encode(digest(c.CERTIFICATE, 'sha256'), 'hex')) FROM root_trust_purpose rtp, ca_certificate cac, certificate c WHERE rtp.TRUST_CONTEXT_ID IN (1, 5, 12) AND rtp.CERTIFICATE_ID = cac.CERTIFICATE_ID AND cac.CERTIFICATE_ID = c.ID GROUP BY digest(c.CERTIFICATE, 'sha256') HAVING min(TRUST_PURPOSE_ID) > 1 ORDER BY min(get_ca_name_attribute(cac.CA_ID))) TO '__roots_sha256_not_serverauth__.sh'"
 
