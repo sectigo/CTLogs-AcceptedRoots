c074caa0daf068a1b7d0fd2df12f74be6a47cb87
Rob Stradling <rob@comodo.com>
2018-02-13 12:59:22 +0000

    Dodo will accept roots in the Java and Android root programs.

--- update_dodo_crt.sh
+++ update_dodo_crt.sh
@@ -3,9 +3,14 @@
 cd crt/dodo/trusted_but_not_for_serverauth
 rm *.crt
 
-# Get a list (from the crt.sh DB) of the SHA-256 hashes of all of the roots that are currently trusted (but not for the server authentication trust purpose) by one or more of the Apple, Microsoft and Mozilla root programs.  These roots will be accepted by Dodo but not by Mammoth or Sabre.
-psql -h crt.sh -p 5432 -U guest certwatch -c "\COPY (SELECT upper(encode(digest(c.CERTIFICATE, 'sha256'), 'hex')) FROM root_trust_purpose rtp, ca_certificate cac, certificate c WHERE rtp.TRUST_CONTEXT_ID IN (1, 5, 12) AND rtp.CERTIFICATE_ID = cac.CERTIFICATE_ID AND cac.CERTIFICATE_ID = c.ID GROUP BY digest(c.CERTIFICATE, 'sha256') HAVING min(TRUST_PURPOSE_ID) > 1 ORDER BY min(get_ca_name_attribute(cac.CA_ID))) TO '__roots_sha256_not_serverauth__.sh'"
+# Get a list (from the crt.sh DB) of the SHA-256 hashes of:
+#   - All of the roots that are currently trusted (but not for the server authentication trust purpose) by one or more of the Apple, Microsoft and Mozilla root programs.
+#   - All of the roots that are currently trusted (for any purpose) by the Java or Android root programs, but not trusted by Apple, Microsoft or Mozilla.
+# These roots will be accepted by Dodo but not by Mammoth or Sabre.
+psql -h crt.sh -p 5432 -U guest certwatch -c "\COPY (SELECT upper(encode(digest(c.CERTIFICATE, 'sha256'), 'hex')) FROM root_trust_purpose rtp, ca_certificate cac, certificate c WHERE rtp.TRUST_CONTEXT_ID IN (1, 5, 12) AND rtp.CERTIFICATE_ID = cac.CERTIFICATE_ID AND cac.CERTIFICATE_ID = c.ID GROUP BY digest(c.CERTIFICATE, 'sha256') HAVING min(TRUST_PURPOSE_ID) > 1 ORDER BY min(get_ca_name_attribute(cac.CA_ID))) TO '__roots_sha256_not_serverauth_part1__.sh'"
+psql -h crt.sh -p 5432 -U guest certwatch -c "\COPY (SELECT upper(encode(digest(c.CERTIFICATE, 'sha256'), 'hex')) FROM root_trust_purpose rtp, ca_certificate cac, certificate c WHERE rtp.TRUST_CONTEXT_ID IN (17, 23) AND NOT EXISTS (SELECT 1 FROM root_trust_purpose rtp2 WHERE rtp2.CERTIFICATE_ID = rtp.CERTIFICATE_ID AND rtp2.TRUST_CONTEXT_ID IN (1, 5, 12)) AND rtp.CERTIFICATE_ID = cac.CERTIFICATE_ID AND cac.CERTIFICATE_ID = c.ID GROUP BY digest(c.CERTIFICATE, 'sha256') ORDER BY min(get_ca_name_attribute(cac.CA_ID))) TO '__roots_sha256_not_serverauth_part2__.sh'"
 
+cat __roots_sha256_not_serverauth_part1__.sh __roots_sha256_not_serverauth_part2__.sh > __roots_sha256_not_serverauth__.sh
 sed -i "s/^/wget --content-disposition \"https:\/\/crt.sh\/?d=/g" __roots_sha256_not_serverauth__.sh
 sed -i "s/$/\"/g" __roots_sha256_not_serverauth__.sh
 chmod 755 __roots_sha256_not_serverauth__.sh
